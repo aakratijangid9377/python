056924683a71e51f68ff33a2e8e13503166c9659
Aakrati jangid <141710221+aakratijangid9377@users.noreply.github.com>
2025-02-01 00:36:28 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,7 @@
+marks = {
+    "aakrati" : 100,
+    "mahendra" : 23,
+    "kalika" : 54,
+}
+# print(marks,type(marks))
+print(marks["aakrati"])
---
+++ unionset.py
@@ -0,0 +1,4 @@
+s1 = {1,4,56,79,69}
+s2 = {1,67,2,7,69}
+# print(s1.union(s2))
+print(s1.intersection(s2))
