ef3d96d84feb2cd8d0adb1b08093b222e9076ac2
aakratijangid9377 <jangidaakrati@gmail.com>
2025-02-02 01:08:06 +0530

    dictmethods.py

---
+++ chapter 5/dictmethods.py
@@ -0,0 +1,10 @@
+marks = {
+    "aakrati" : 100,
+    "mahendra" : 23,
+    "kalika" : 54,
+}
+# print(marks.items())
+# print(marks.values())
+# print(marks.keys())
+marks.update({"aakrati" : 100, "harsh" : 45})
+print(marks)
