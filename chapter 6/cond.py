ddc18564f6dfa821de92185a2bdac3d375f1b2ea
aakratijangid9377 <jangidaakrati@gmail.com>
2025-02-02 01:04:49 +0530

    cond.py

---
+++ chapter 6/cond.py
@@ -0,0 +1,13 @@
+a = int(input("enter you age : "))
+
+if(a>18):
+    print("you can vote")
+
+elif(a<0):
+    print("you entered invalid age : ")
+    
+else:    
+    print("you cant vote")    
+
+print("end of the program")
+        
