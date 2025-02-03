2719b5e49ac3708119cd28ab14d4d0a2edee0467
aakratijangid9377 <jangidaakrati@gmail.com>
2025-02-04 01:13:33 +0530

    break_and_cont.py

---
+++ chapter 7/break_and_cont.py
@@ -0,0 +1,13 @@
+# for i in range(100):
+#         if(i == 34):
+#             break
+
+# print(i)
+
+
+
+
+for i in range(100):
+    if i == 34:
+        continue  # Skips printing 34
+    print(i)
