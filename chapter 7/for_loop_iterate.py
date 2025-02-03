49e3440ec6182221bf333b1a6d020b5355f1354e
aakratijangid9377 <jangidaakrati@gmail.com>
2025-02-04 01:14:00 +0530

    for_loop_iterate.py

---
+++ chapter 7/for_loop_iterate.py
@@ -0,0 +1,18 @@
+l = ["1", "aakrati", "kalika", "mahendra", "345"]
+
+for i in l:
+    print(i)
+
+
+# for loop for tuple
+t = (1,34,56,256,2453)
+
+for i in t:
+    print(i)
+
+
+# for loop for strings
+s = "aakrati"
+
+for i in s:
+    print(i)
